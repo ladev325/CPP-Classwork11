d76fe3aeb6f49f755ce9603a6aac7c55aba1d0d8
ladev325 <vlad2008artemov@gmail.com>
2025-07-01 17:13:08 +0300

    E

--- CPP-Classwork11/Diary.cpp
+++ CPP-Classwork11/Diary.cpp
@@ -10,10 +10,18 @@ Diary::Diary()
 
 void Diary::addEvent(Date d, string description)
 {
+	records[d].push_back(description);
 }
 
 void Diary::showAll() const
 {
+	for (auto item : records) {
+		cout << "[" << item.first << "] - " << endl;
+		for (int i = 0; i < item.second.size(); i++) {
+			cout << "\t" << item.second[i] << endl;
+		}
+	}
+	cout << endl;
 }
 
 void Diary::findByDate(Date d) const
